4fc05e258df3a583cb8bd83dba36fd9f90177ad1
EJLarcs <ejlarcombe@gmail.com>
2015-09-16 13:44:26 -0400

    dataType not type ... its my fault

--- js/spotify-chart.js
+++ js/spotify-chart.js
@@ -14,19 +14,46 @@ $(function() {
 
 function extractTop20Tracks(tracks) {
   // your code here
+  return tracks.slice(0,20);
+
 }
+
 
 function extractNumberOfStreams(tracks) {
   // your code here
+  // look up .map in js
+  var numStreamsTracks = [];
+    for (var i = 0; i < tracks.length; i++) {
+      numStreamsTracks.push(tracks[i].num_streams);
+    }
+  return numStreamsTracks;
+
 }
 
 function extractNames(tracks) {
   // your code here
+    var nameOfTracks = [];
+    for (var i = 0; i < tracks.length; i++) {
+      nameOfTracks.push(tracks[i].track_name);
+    }
+  return nameOfTracks;
 }
 
 function chartData(labels, inputData) {
   // your code here
-
+    dataSetProperties.data = inputData
+    return {
+      labels: labels,
+      datasets: [dataSetProperties]
+      // datasets: [{
+      //   label: 'Spotify Chart of Top 20 Streamed Songs on Spotify with their Steam Count', 
+      //   fillColor: 'rgba(220,220,220,0.5)', 
+      //   strokeColor: 'rgba(220,220,220,0.8)', 
+      //   highlightFill: 'rgba(220,220,220,0.75)', 
+      //   highlightStroke: 'rgba(220,220,220,1)',
+      //   data: inputData
+      // }]
+    };
   // use the dataSetProperties variable defined above if it helps
 }
 
@@ -34,19 +61,33 @@ function getSpotifyTracks(callback){
   // your ajax call here, on success it should call on the 
   // parameter it's passed (it's a function), and pass it's 
   // parameter the data it received
-
+     $.ajax({
+      url: url,
+      dataType: 'jsonp',
+      success: callback
+    });
   // use the url variable defined above if it helps
 }
 
 function success(parsedJSON) {
+ 
   // this function will make a new bar chart, refer to this url:
   // http://www.chartjs.org/docs/#bar-chart
   // you will need to call on:
-  //  1. extractTop20Tracks - pass it tracks
+
+   // 1. extractTop20Tracks - pass it tracks
+  var tracks = extractTop20Tracks(parsedJSON.tracks);
   //  2. extractNames -  pass it the result of #1
-  //  3. extractNumberOfStreams - pass it the result of #1
-  //  4. chartData - pass it results of #2 and #3
-  //  5. make a variable `ctx` and select the canvas with the id of spotify-chart
+  var names = extractNames(tracks);
+    //  3. extractNumberOfStreams - pass it the result of #1
+  var streams = extractNumberOfStreams(tracks);
+    //  4. chartData - pass it results of #2 and #3
+  var data = chartData(names, streams);
+   //  5. make a variable `ctx` and select the canvas with the id of spotify-chart 
   //     * also make sure to specify 2d context
+  var ctx = $('#spotify-chart').get(0).getContext("2d");
+  //returns array 
   //  6. make a new bar chart!
+  new Chart(ctx).Bar(data);
+
 }
